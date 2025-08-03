80258b572b84609ec0efb6c9221d84e0b4ce886e
Yasmin-tota <79907631+Yasmin-tota@users.noreply.github.com>
2025-08-03 23:58:25 +0300

    add padding

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,7 @@ class BusinessCard extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xff2A4158),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
+        body: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
               //  ClipRRect(
@@ -39,15 +36,84 @@ class BusinessCard extends StatelessWidget {
                 'Yasmeen Bashandy',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize:32, 
+                  fontSize:30, 
                   fontFamily: 'Pacifico',
                 ),
-                
-                )
+                ),
+                Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  color:Color(0xFF6C8090),
+                  fontSize:18, 
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+                Padding(
+                  padding:EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    height: 65,
+                    // width: 60,
+                    // height: 60,
+                    child: Row(
+                      children: [
+                        
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Icon(Icons.phone,
+                          size: 32,
+                          color: Color(0xff2A4158)
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left:22),
+                          child: Text('(+2) 1026699108',
+                          style: TextStyle(
+                            fontSize: 24)
+                            ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    height: 65,
+                    // width: 60,
+                    // height: 60,
+                    child: Row(
+                      children: [
+                        
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Icon(Icons.mail,
+                          size: 32,
+                          color: Color(0xff2A4158)
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left:22),
+                          child: Text('yasmeenahmed',
+                          style: TextStyle(
+                            fontSize: 24)
+                            ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              
+              
               ],
-            ),
-          ],
-        ),
+            ), 
       ),
     );
   }
